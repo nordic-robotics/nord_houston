57b7b4408bac8fbd4c1c9c880879a76948fc07ab
Lucas Åström <lucasas@kth.se>
2015-10-25 21:06:51 +0100

    basic publishing

--- src/wall_following.cpp
+++ src/wall_following.cpp
@@ -1,24 +1,37 @@
 #include "ros/ros.h"
 #include <cstdint>
 #include <iostream>
+#include <vector>
+#include "nord_messages/Command.h"
 
-enum class action : uint16_t
+enum class action : uint32_t
 {
+    none = 0,
     forwards_speed,
     backwards_distance,
     turn_left,
     turn_right
 };
 
+bool publish(action, const std::vector<float>&);
 bool publish(action, float);
 
 #include "seqsel/WallFollow.hpp"
 
-bool publish(action a, float f)
+ros::Publisher mission_control;
+
+bool publish(action a, const std::vector<float>& values)
 {
+    nord_messages::Command msg;
+    msg.action = uint32_t(a);
+    msg.values = values;
+    mission_control.publish(msg);
     return true;
 }
-
+bool publish(action a, float value)
+{
+    return publish(a, std::vector<float>({value}));
+}
 
 int main(int argc, char** argv)
 {
@@ -26,7 +39,16 @@ int main(int argc, char** argv)
     ros::NodeHandle n;
 
     WallFollow wf;
-    std::cout << wf.run() << std::endl;
+
+    mission_control = n.advertise<nord_messages::Command>("/nord/houston/mission_control", 10);
+
+    ros::Rate r(10);
+    while (ros::ok())
+    {
+        ros::spinOnce();
+        wf.run();
+        r.sleep();
+    }
 
     return 0;
 }
