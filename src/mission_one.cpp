6ae4c74611fa0eb6848b6a04cefe9773b6c39a72
Lucas Åström <lucasas@kth.se>
2015-11-25 22:02:41 +0100

    compile errors and message type for go_to

--- src/mission_one.cpp
+++ src/mission_one.cpp
@@ -10,12 +10,36 @@
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "nord_houston_wall_following");
+    ros::init(argc, argv, "nord_houston_mission_one");
     ros::NodeHandle n;
 
-    mission_one_behaviour<MissionOne> b(n);
+    std::vector<point<2>> path({
+        point<2>(0.17, 2.1),
+        point<2>(0.17, 1.5),
+        point<2>(0.17, 1.04),
+        point<2>(0.5, 1.04),
+        point<2>(0.99, 1.04),
+        point<2>(0.99, 1.5),
+        point<2>(0.99, 2.11),
+        point<2>(0.17, 2.1),
+        point<2>(0.17, 1.5),
+        point<2>(0.17, 1.04),
+        point<2>(0.5, 1.04),
+        point<2>(0.99, 1.04),
+        point<2>(0.99, 1.5),
+        point<2>(0.99, 2.11),
+        point<2>(0.17, 2.1),
+        point<2>(0.17, 1.5),
+        point<2>(0.17, 1.04),
+        point<2>(0.5, 1.04),
+        point<2>(0.99, 1.04),
+        point<2>(0.99, 1.5),
+        point<2>(0.99, 2.11)
+    });
 
+    mission_one_behaviour<MissionOne> b(n, path);
     b.behave();
+
 
     return 0;
 }
--- src/mission_one_behaviour.hpp
+++ src/mission_one_behaviour.hpp
@@ -3,6 +3,7 @@
 #include "behaviour.hpp"
 #include "nord_messages/PoseEstimate.h"
 #include "nord_messages/Vector2.h"
+#include "nord_messages/NextNode.h"
 #include "nord_messages/ClassificationSrv.h"
 #include "nord_messages/ObjectArray.h"
 #include "std_msgs/String.h"
@@ -15,31 +16,35 @@ template<class BT>
 class mission_one_behaviour : public behaviour<BT, mission_one_behaviour<BT>>
 {
     using base = behaviour<BT, mission_one_behaviour>;
+    using base::tree;
 public:
-    mission_one_behaviour(ros::NodeHandle& n) : n(n), base(*this, n)
+    mission_one_behaviour(ros::NodeHandle& n, const std::vector<point<2>> path)
+        : n(n), base(*this, n)
     {
         this->template subscribe<nord_messages::PoseEstimate>("/nord/estimation/gaussian", 9,
             [&](const nord_messages::PoseEstimate::ConstPtr& msg) {
-                base::tree.pose = *msg;
+                tree.pose = *msg;
                 sort_unknown();
             });
         this->template subscribe<nord_messages::ObjectArray>("/nord/estimation/objects", 9,
             [&](const nord_messages::ObjectArray::ConstPtr& msg) {
-                base::tree.unknown.clear();
-                base::tree.unknown.reserve(msg->data.size());
+                tree.unknown.clear();
+                tree.unknown.reserve(msg->data.size());
                 for (auto& d : msg->data)
                 {
-                    base::tree.unknown.push_back(std::make_pair(d.id, point<2>(d.x, d.y)));
+                    tree.unknown.push_back(std::make_pair(d.id, point<2>(d.x, d.y)));
                 }
             });
-        this->template advertise<nord_messages::Vector2>("/nord/control/point", 10);
+        this->template advertise<nord_messages::NextNode>("/nord/control/point", 10);
         this->template advertise<std_msgs::String>("/espeak/string", 10);
 
         tick = n.createTimer(ros::Duration(1), [&](const ros::TimerEvent& e) {
-            base::tree.time_left -= 1;
+            tree.time_left -= 1;
         });
 
-        base::tree.time_left = 60 * 5;
+        tree.time_left = 60 * 5;
+
+        tree.path = path;
     }
 
     float distance_to_exit_heuristic()
@@ -55,12 +60,12 @@ public:
 
     float distance_to(const point<2>& target)
     {
-        return (target - point<2>(base::tree.pose.x.mean, base::tree.pose.y.mean)).length();
+        return (target - point<2>(tree.pose.x.mean, tree.pose.y.mean)).length();
     }
 
     void sort_unknown()
     {
-        std::sort(base::tree.unknown.begin(), base::tree.unknown.end(),
+        std::sort(tree.unknown.begin(), tree.unknown.end(),
             [&](const std::pair<size_t, point<2>>& a,
                const std::pair<size_t, point<2>>& b) {
                    return distance_to(a.second) < distance_to(b.second);
@@ -72,10 +77,10 @@ public:
         std::cout << "align" << std::endl;
         // TODO: make sure this always picks a new point to prevent infinite loops
         // maybe consult map?
-        point<2> current(base::tree.pose.x.mean, base::tree.pose.y.mean);
+        point<2> current(tree.pose.x.mean, tree.pose.y.mean);
         point<2> dir = target - current;
         point<2> aligned;
-        if (dir.length() > base::tree.align_range)
+        if (dir.length() > tree.align_range)
         {
             aligned = target + (-dir).normalized();
             return go_to(aligned);
@@ -103,13 +108,13 @@ public:
             return false;
         }
 
-        base::tree.unknown.erase(std::remove_if(base::tree.unknown.begin(),
-                                                base::tree.unknown.end(),
+        tree.unknown.erase(std::remove_if(tree.unknown.begin(),
+                                                tree.unknown.end(),
             [&](const std::pair<size_t, point<2>>& p) {
                 return p.first == id;
             }));
 
-        base::tree.classified.push_back(new_class);
+        tree.classified.push_back(new_class);
 
         return true;
     }
@@ -117,27 +122,28 @@ public:
     bool announce()
     {
         std::cout << "announce" << std::endl;
-        std::transform(base::tree.classified.begin(), base::tree.classified.end(),
-                       std::back_inserter(base::tree.announced),
+        std::transform(tree.classified.begin(), tree.classified.end(),
+                       std::back_inserter(tree.announced),
                        [&](const std::string& name) {
                            std_msgs::String msg;
                            msg.data = name;
                            std::cout << "\t" << msg.data << std::endl;
-                           base::publish("/espeak/string", msg);
+                           this->template publish("/espeak/string", msg);
                            return name;
                        });
-        base::tree.classified.clear();
+        tree.classified.clear();
         return true;
     }
 
-    bool go_to(const point<2>& p)
+    bool go_to(const point<2>& p, int move = 1)
     {
         std::cout << "go_to (" << p.x() << ", " << p.y() << ")" << std::endl;
-        nord_messages::Vector2 msg;
+        nord_messages::NextNode msg;
         msg.x = p.x();
         msg.y = p.y();
+        msg.move = move;
 
-        return base::publish("/nord/control/point", msg, true);
+        return this->template publish("/nord/control/point", msg, true);
     }
 
 private:
