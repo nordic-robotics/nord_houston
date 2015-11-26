48af8c0d0e58ce900c2882df3251f86b31336784
Forrest Gump <vahiki123@gmail.com>
2015-11-26 12:30:57 +0100

    entire maze run

--- src/mission_one.cpp
+++ src/mission_one.cpp
@@ -15,7 +15,7 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "nord_houston_mission_one");
     ros::NodeHandle n;
 
-    std::vector<point<2>> path({
+    std::vector<point<2>> path_U({
         point<2>(0.17, 2.1),
         point<2>(0.17, 1.5),
         point<2>(0.17, 1.04),
@@ -39,7 +39,28 @@ int main(int argc, char** argv)
         point<2>(0.99, 2.11)
     });
 
-    mission_one_behaviour<MissionOne> b(n, path);
+    std::vector<point<2>> path_maze({
+       // point<2>(0.15, 0.23),
+        point<2>(2.22, 0.23),
+        point<2>(2.22, 0.68),
+        point<2>(1.83, 0.68),
+        point<2>(1.83, 1.06),
+        point<2>(2.22, 1.06),
+        point<2>(2.22, 1.53),
+        point<2>(1.83, 1.53),
+        point<2>(1.83, 2.22),
+        point<2>(1.01, 2.22),
+        point<2>(0.17, 2.1),
+        point<2>(0.17, 1.5),
+        point<2>(0.17, 1.04),
+        point<2>(0.5, 1.04),
+        point<2>(0.99, 1.04),
+        point<2>(0.99, 1.5),
+        point<2>(0.99, 2.11),
+        point<2>(0.17, 2.1)
+    });
+
+    mission_one_behaviour<MissionOne> b(n, path_maze);
 
     std::this_thread::sleep_for(2s);
     b.behave();
--- src/mission_one_behaviour.hpp
+++ src/mission_one_behaviour.hpp
@@ -5,6 +5,7 @@
 #include "nord_messages/Vector2.h"
 #include "nord_messages/NextNode.h"
 #include "nord_messages/ClassificationSrv.h"
+#include "nord_messages/EvidenceSrv.h"
 #include "nord_messages/ObjectArray.h"
 #include "std_msgs/String.h"
 #include "point.hpp"
@@ -26,27 +27,35 @@ public:
                 tree.pose = *msg;
                 sort_unknown();
             });
-        int num_objects = 0;
         this->template subscribe<nord_messages::ObjectArray>("/nord/estimation/objects", 9,
             [&](const nord_messages::ObjectArray::ConstPtr& msg) {
-                tree.unknown.clear();
-                tree.unknown.reserve(msg->data.size());
-                int new_num_objects = 0;
+                bool should_abort = false;
                 for (auto& d : msg->data)
                 {
-                    new_num_objects++;
-                    if (std::find(classified_history.begin(),
-                                  classified_history.end(),
-                                  d.id) == classified_history.end())
+                    if (std::find_if(tree.unknown.begin(),
+                                  tree.unknown.end(),
+                                  [&](const std::pair<size_t, point<2>>& thing) {
+                                    return thing.first == d.id;
+                                  }) == tree.unknown.end())
                     {
                         tree.unknown.push_back(std::make_pair(d.id, point<2>(d.x, d.y)));
+                        should_abort = true;
                     }
                 }
-                if (new_num_objects > num_objects)
+                if (should_abort)
                 {
-                    num_objects = new_num_objects;
+                    std::cout << "found new object" << std::endl;
                     this->template abort();
                 }
+            });
+        this->template subscribe<nord_messages::Vector2>("/imu/bump", 9,
+            [&](const nord_messages::Vector2::ConstPtr& msg) {
+                std::cout << "BUMP!" << std::endl;
+                go_to(point<2>(tree.pose.x.mean, tree.pose.y.mean)
+                    + point<2>(std::cos(tree.pose.theta.mean),
+                               std::sin(tree.pose.theta.mean)) * 0.05,
+                      2);
+                this->template abort();
             });
         this->template advertise<nord_messages::NextNode>("/nord/control/point", 10);
         this->template advertise<std_msgs::String>("/espeak/string", 10);
@@ -93,15 +102,9 @@ public:
         // TODO: make sure this always picks a new point to prevent infinite loops
         // maybe consult map?
         point<2> current(tree.pose.x.mean, tree.pose.y.mean);
-        point<2> dir = target - current;
+        //point<2> dir = target - current;
         point<2> aligned;
-        go_to(current, 0);
-        if (dir.length() > tree.align_range && false)
-        {
-            aligned = target + (-dir).normalized();
-            return go_to(aligned);
-        }
-        return true;
+        return true;//go_to(target, 3);;
     }
 
     bool classify(size_t id)
@@ -124,13 +127,32 @@ public:
             return false;
         }
 
-        tree.unknown.erase(std::remove_if(tree.unknown.begin(),
-                                          tree.unknown.end(),
-            [&](const std::pair<size_t, point<2>>& p) {
-                return p.first == id;
-            }), tree.unknown.end());
+        point<2> obj_location(-1, -1);
+        for (size_t i = 0; i < tree.unknown.size(); i++)
+        {
+            if (tree.unknown[i].first == id)
+            {
+                obj_location = tree.unknown[i].second;
+                tree.unknown.erase(tree.unknown.begin() + i);
+                i--;
+            }
+        }
 
         tree.classified.push_back(new_class);
+
+        std::cout << "snapping evidence" << std::endl;
+        ros::ServiceClient client2 = n.serviceClient<nord_messages::EvidenceSrv>(
+            "/nord/evidence_service", false);
+        nord_messages::EvidenceSrv srv2;
+        // fill in
+        srv2.request.id = id;
+        srv2.request.position.x = obj_location.x();
+        srv2.request.position.y = obj_location.y();
+        srv2.request.classification.data = new_class;
+        if (!client2.call(srv2))
+        {
+            std::cout << "evidence call failed!" << std::endl;
+        }
 
         return true;
     }
@@ -153,7 +175,7 @@ public:
 
     bool go_to(const point<2>& p, int move = 1)
     {
-        std::cout << "go_to (" << p.x() << ", " << p.y() << ")" << std::endl;
+        std::cout << "go_to (" << p.x() << ", " << p.y() << ")" << move << std::endl;
         nord_messages::NextNode msg;
         msg.x = p.x();
         msg.y = p.y();
